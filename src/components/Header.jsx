a124494d172d29fe1463fe62a1871e72e7b5118a
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-15 23:26:36 +0530

    feat: updated header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,25 +10,29 @@ const Header = ( props ) => {
             <ContainerJSX>
                 <nav className="flex items-center justify-between">
                     <div className={` ${ dark ? "hidden" : "block" } w-64 aspect-video`}>
-                        <img
-                            src="/logos/ui_light_logo.svg"
-                            alt=""
-                            className="w-full h-full"
-                        />
+                        <a href="/">
+                            <img
+                                src="/logos/ui_light_logo.svg"
+                                alt=""
+                                className="w-64 aspect-video"
+                            />
+                        </a>
                     </div>
                     <div className={` ${ dark ? "block" : "hidden" } w-64 aspect-video`}>
-                        <img
-                            src="/logos/ui_logo.svg"
-                            alt=""
-                            className="w-full h-full"
-                        />
+                        <a href="/">
+                            <img
+                                src="/logos/ui_logo.svg"
+                                alt=""
+                                className="w-full aspect-video"
+                            />
+                        </a>
                     </div>
                     <div className="flex gap-10 items-center">
-                        <ul className="flex">
-                            <HeaderItemJSX dark={ true }>What we do</HeaderItemJSX>
-                            <HeaderItemJSX dark={ true }>Work</HeaderItemJSX>
-                            <HeaderItemJSX dark={ true }>About Us</HeaderItemJSX>
-                            <HeaderItemJSX dark={ true }>Articles</HeaderItemJSX>
+                        <ul className={` ${ dark ? "text-black" : "text-white" } flex`}>
+                            <HeaderItemJSX>What we do</HeaderItemJSX>
+                            <HeaderItemJSX>Work</HeaderItemJSX>
+                            <HeaderItemJSX>About Us</HeaderItemJSX>
+                            <HeaderItemJSX>Articles</HeaderItemJSX>
                         </ul>
                         <button className={` ${ dark ? "text-black border-black" : "text-white border-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase border py-7 px-6 rounded`}>
                             Contact Us
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,9 +1,7 @@
 const HeaderItemJSX = ( props ) => {
 
-    const { dark } = props
-
     return(
-        <li className={` ${ dark ? "text-black" : "text-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase px-4 py-4 transition-all duration-200 hover:cursor-pointer`}>
+        <li className="text-sm hover:text-urbaninsight_light_gray font-bold uppercase px-4 py-4 transition-all duration-200 hover:cursor-pointer">
             { props.children }
         </li>
     )
