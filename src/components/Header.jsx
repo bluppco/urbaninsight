2558fe1ba118e411a74e8d0f536eeb61e5af3de0
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-16 01:19:11 +0530

    x

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ const Header = ( props ) => {
             <ContainerJSX>
                 <nav className="flex items-center justify-between">
                     <div className={` ${ dark ? "hidden" : "block" } w-64 aspect-video`}>
+<<<<<<< HEAD
                         <a href="/">
                             <img
                                 src="/logos/ui_light_logo.svg"
@@ -26,13 +27,35 @@ const Header = ( props ) => {
                                 className="w-full aspect-video"
                             />
                         </a>
+=======
+                        <img
+                            src="/logos/ui_light_logo.svg"
+                            alt=""
+                            className="w-full h-full"
+                        />
+>>>>>>> 9f1336e (feat: added dark variant of header)
+                    </div>
+                    <div className={` ${ dark ? "block" : "hidden" } w-64 aspect-video`}>
+                        <img
+                            src="/logos/ui_logo.svg"
+                            alt=""
+                            className="w-full h-full"
+                        />
                     </div>
                     <div className="flex gap-10 items-center">
+<<<<<<< HEAD
                         <ul className={` ${ dark ? "text-black" : "text-white" } flex`}>
                             <HeaderItemJSX>What we do</HeaderItemJSX>
                             <HeaderItemJSX>Work</HeaderItemJSX>
                             <HeaderItemJSX>About Us</HeaderItemJSX>
                             <HeaderItemJSX>Articles</HeaderItemJSX>
+=======
+                        <ul className="flex">
+                            <HeaderItemJSX dark={ true }>What we do</HeaderItemJSX>
+                            <HeaderItemJSX dark={ true }>Work</HeaderItemJSX>
+                            <HeaderItemJSX dark={ true }>About Us</HeaderItemJSX>
+                            <HeaderItemJSX dark={ true }>Articles</HeaderItemJSX>
+>>>>>>> 9f1336e (feat: added dark variant of header)
                         </ul>
                         <button className={` ${ dark ? "text-black border-black" : "text-white border-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase border py-7 px-6 rounded`}>
                             Contact Us
