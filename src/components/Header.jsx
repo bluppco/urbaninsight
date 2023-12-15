985da81123822cde4d3236df36a28d1645f40f37
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-15 15:20:55 +0530

    feat: updated header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import HeaderItemJSX from "./HeaderItem"
 const Header = () => {
 
     return(
-        <header className="h-28 bg-white flex items-center shadow-xl fixed top-0 w-full z-50">
+        <header className="h-[136px] flex items-center top-0 w-full z-50">
             <ContainerJSX>
                 <nav className="flex items-center justify-between">
                     <div className="w-64 aspect-video">
                         <img
-                            src="/logos/ui_logo.svg"
+                            src="/logos/ui_light_logo.svg"
                             alt=""
                             className="w-full h-full"
                         />
@@ -21,7 +21,7 @@ const Header = () => {
                             <HeaderItemJSX>About Us</HeaderItemJSX>
                             <HeaderItemJSX>Articles</HeaderItemJSX>
                         </ul>
-                        <button className="text-sm text-urbaninsight_black hover:text-urbaninsight_light_gray font-bold uppercase border border-urbaninsight_black py-7 px-6 rounded">
+                        <button className="text-sm text-white hover:text-urbaninsight_light_gray font-bold uppercase border border-white py-7 px-6 rounded">
                             Contact Us
                         </button>
                     </div>
