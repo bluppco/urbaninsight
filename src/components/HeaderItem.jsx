5f38df7cb5a8259a2075cdebaabe980d989260b4
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-15 23:15:56 +0530

    feat: added dark variant of header

--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,7 +1,9 @@
 const HeaderItemJSX = ( props ) => {
 
+    const { dark } = props
+
     return(
-        <li className="text-sm text-white hover:text-urbaninsight_light_gray font-bold uppercase px-4 py-4 transition-all duration-200 hover:cursor-pointer">
+        <li className={` ${ dark ? "text-black" : "text-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase px-4 py-4 transition-all duration-200 hover:cursor-pointer`}>
             { props.children }
         </li>
     )
