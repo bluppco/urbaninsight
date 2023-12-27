d5f7f683701bfa752407885a33981adf0c7ca359
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-27 10:47:46 +0530

    feat: added animation in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,95 @@
 import ContainerJSX from "../layouts/Container"
 import HeaderItemJSX from "./HeaderItem"
 
-const Header = ( props ) => {
+// IMPORT FRAMER MOTION LIBRARY
+import { motion, useScroll, useAnimation, useMotionValueEvent } from "framer-motion"
 
-    const { dark } = props
+// IMPORT USESTATE AND USEFFECT
+import { useState, useEffect } from "react"
+
+const Header = () => {
+
+    const [ isScrolled, setIsScrolled ] = useState( false )
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+
+          setIsScrolled( window.scrollY > 100 )
+
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener("scroll", handleScroll)
+
+    }, [])
+
+	const { scrollY } = useScroll()
+    const squareVariants = {
+
+        display: { y: 0, transition: { duration: .2 } },
+        hide: { y: "-100%", transition: { duration: .4 } },
+
+    }
+    const controls = useAnimation( scrollY )
+    useMotionValueEvent(scrollY, "change", (latest) => {
+
+        let isScrollingDown = scrollY.getPrevious() - latest < 0;
+        if( isScrollingDown && latest > 0 ){
+
+            controls.start("hide")
+
+        } else {
+
+            controls.start("display")
+
+        }
+
+
+    })
 
     return(
-        <header className="hidden md:flex items-center h-[136px] top-0 w-full z-50">
-            <ContainerJSX>
-                <nav className="flex items-center justify-between">
-                    <div className={` ${ dark ? "hidden" : "block" } w-64 aspect-video`}>
-                        <a href="/">
-                            <img
-                                src="/logos/ui_light_logo.svg"
-                                alt=""
-                                className="w-64 aspect-video"
-                            />
-                        </a>
-                    </div>
-                    <div className={` ${ dark ? "block" : "hidden" } w-64 aspect-video`}>
-                        <a href="/">
-                            <img
-                                src="/logos/ui_logo.svg"
-                                alt=""
-                                className="w-full aspect-video"
-                            />
-                        </a>
-                    </div>
-                    <div className="flex gap-10 items-center">
-                        <ul className={` ${ dark ? "text-black" : "text-white" } flex`}>
-                            <HeaderItemJSX>What we do</HeaderItemJSX>
-                            <HeaderItemJSX>Work</HeaderItemJSX>
-                            <HeaderItemJSX>About Us</HeaderItemJSX>
-                            <HeaderItemJSX>Articles</HeaderItemJSX>
-                        </ul>
-                        <button className={` ${ dark ? "text-black border-black" : "text-white border-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase border py-7 px-6 rounded`}>
-                            Contact Us
-                        </button>
-                    </div>
-                </nav>
-            </ContainerJSX>
+        <header className="hidden md:block">
+            <motion.header className={` ${ isScrolled ? "bg-black" : "bg-transparent" } h-32 w-full fixed top-4 z-50`}
+                variants={ squareVariants }
+                initial="display"
+                animate={ controls }
+            >
+                <ContainerJSX>
+                    <nav className="flex items-center justify-between">
+                        <div className={` ${ isScrolled ? "hidden" : "block" } w-64 aspect-video`}>
+                            <a href="/">
+                                <img
+                                    src="/logos/ui_light_logo.svg"
+                                    alt=""
+                                    className="w-64 aspect-video"
+                                />
+                            </a>
+                        </div>
+                        <div className={` ${ isScrolled ? "block" : "hidden" } w-64 aspect-video`}>
+                            <a href="/">
+                                <img
+                                    src="/logos/ui_logo.svg"
+                                    alt=""
+                                    className="w-full aspect-video"
+                                />
+                            </a>
+                        </div>
+                        <div className="flex gap-10 items-center">
+                            <ul className={` ${ isScrolled ? "text-black" : "text-white" } flex`}>
+                                <HeaderItemJSX>What we do</HeaderItemJSX>
+                                <HeaderItemJSX>Work</HeaderItemJSX>
+                                <HeaderItemJSX>About Us</HeaderItemJSX>
+                                <HeaderItemJSX>Articles</HeaderItemJSX>
+                            </ul>
+                            <button className={` ${ isScrolled ? "text-black border-black" : "text-white border-white" } text-sm hover:text-urbaninsight_light_gray font-bold uppercase border py-7 px-6 rounded`}>
+                                Contact Us
+                            </button>
+                        </div>
+                    </nav>
+                </ContainerJSX>
+            </motion.header>
         </header>
     )
 
